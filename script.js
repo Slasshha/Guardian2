920ef5a4d8f5f7b32703a1414f70543719f76f09
Oleksandra <Slasshha@users.noreply.github.com>
2016-07-11 00:11:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+// Code goes here
+(function() {
+  var app = angular.module("newsViewer", []);
+
+
+  var MainController = function($scope, $http) {
+
+    var newsDownloaded = function(response) {
+      $scope.myData = response;
+      console.log(response);
+      $scope.numPages = response.data.response.pages;
+      $scope.currentPage = response.data.response.currentPage;
+      $scope.newsList = response.data.response.results;
+
+      console.log($scope.newsList.length);
+
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Sorry, we couldn't find news for you. Please try again later."
+    };
+
+
+
+    var getNews = function(reqPage) {
+
+      $http.get("https://content.guardianapis.com/search?api-key=test&page=" + reqPage)
+        .then(newsDownloaded, onError);
+
+
+    };
+
+    getNews(1);
+    
+    
+    $scope.updateNewsList = function(keyEvent) {
+  if (keyEvent.which === 13)
+   getNews($scope.currentPage);
+}
+
+
+    
+
+  };
+
+
+
+  app.controller("MainController", ["$scope", "$http", MainController]);
+
+
+
+}());
