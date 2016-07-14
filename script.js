b6fc6088259bf4577aa7c0ec6115db79c13673f8
Oleksandra Puravets <o.puravets@levi9.com>
2016-07-14 15:44:55 +0300

    Updated styles; check for input

--- script.js
+++ script.js
@@ -1,66 +1,74 @@
-// Code goes here
+
 (function() {
-  var app = angular.module("newsViewer", []);
+    var app = angular.module("newsViewer", []);
 
 
-  var MainController = function($scope, $http) {
+    var MainController = function($scope, $http) {
 
-    var newsDownloaded = function(response) {
-      $scope.myData = response;
-      console.log(response);
-      $scope.numPages = response.data.response.pages;
-      $scope.currentPage = response.data.response.currentPage;
-      $scope.newsList = response.data.response.results;
+        var newsDownloaded = function(response) {
+            $scope.myData = response;
+            console.log(response);
+            $scope.numPages = response.data.response.pages;
+            $scope.currentPage = response.data.response.currentPage;
+            $scope.newsList = response.data.response.results;
 
-      console.log($scope.newsList.length);
+            console.log($scope.newsList.length);
+
+        };
+
+        var onError = function(reason) {
+            $scope.error = "Sorry, we couldn't find news for you. Please try again later."
+        };
+
+
+
+        var getNews = function(reqPage) {
+
+            $http.get("https://content.guardianapis.com/search?api-key=test&page=" + reqPage)
+                .then(newsDownloaded, onError);
+
+
+        };
+
+        getNews(1);
+
+
+        $scope.updateNewsList = function(Event) {
+
+            if ( ! angular.isNumber($scope.currentPage) ) {
+                $scope.error = $scope.currentPage + " is not a valid page number.";
+                console.log(angular.isNumber($scope.currentPage) );
+            } else
+            if ($scope.currentPage < 1 || $scope.currentPage > $scope.numPages) {
+                $scope.error = "Page number should be within the range 1-" + $scope.numPages;
+            } else if (Event.which === 13) {
+                $scope.error = "";
+                getNews($scope.currentPage);
+            }
+
+        };
+
+
+        $scope.updateNewsPrevious = function() {
+            $scope.currentPage = $scope.currentPage - 1;
+            getNews($scope.currentPage);
+
+        };
+
+        $scope.updateNewsNext = function() {
+            $scope.currentPage = $scope.currentPage + 1;
+            getNews($scope.currentPage);
+
+        };
+
+
+
 
     };
 
-    var onError = function(reason) {
-      $scope.error = "Sorry, we couldn't find news for you. Please try again later."
-    };
 
 
-
-    var getNews = function(reqPage) {
-
-      $http.get("https://content.guardianapis.com/search?api-key=test&page=" + reqPage)
-        .then(newsDownloaded, onError);
-
-
-    };
-
-    getNews(1);
-     getNews(1);
-    
-    
-    $scope.updateNewsList = function(Event) {
-    if (Event.which === 13)
-    getNews($scope.currentPage);
-    };
-
-
-    $scope.updateNewsPrevious = function() {
-      $scope.currentPage = $scope.currentPage-1;
-      getNews($scope.currentPage);
-
-    };
-
-    $scope.updateNewsNext = function() {
-      $scope.currentPage = $scope.currentPage+1;
-      getNews($scope.currentPage);
-
-    };
-
-
-   
-    
-
-  };
-
-
-
-  app.controller("MainController", ["$scope", "$http", MainController]);
+    app.controller("MainController", ["$scope", "$http", MainController]);
 
 
 
